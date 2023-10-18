8f36203ae03fc25dadc3cc886d0bca767667aa78
MarioMartinez00072520 <00072520@uca.edu.sv>
2023-10-18 09:56:12 -0600

    :heavy_plus_sign: add(dependency): added cors to project :heavy_plus_sign:

--- sed-api/app.js
+++ sed-api/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./src/routes/api/index');
 var usersRouter = require('./src/routes/api/users');
@@ -14,7 +15,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+app.use(cors());
 
 module.exports = app;
